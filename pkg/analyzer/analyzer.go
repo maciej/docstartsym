37d5d98b3461e8017079b9d7ed8a96fb5083b56e
Maciej Biłas <maciej@bilas.io>
2025-04-08 15:48:49 +0200

    feat: extend doc comment validation to types, vars and consts
    - Add support for checking doc comments on type, variable and constant declarations
    - Improve error messages to specify symbol type
    - Add analyzer URL to documentation
    - Update test cases

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -2,6 +2,7 @@ package analyzer
 
 import (
 	"go/ast"
+	"go/token"
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
@@ -9,31 +10,75 @@ import (
 
 var Analyzer = &analysis.Analyzer{
 	Name: "docstartsym",
-	Doc:  "check that documentation starts with the symbol name",
+	Doc:  "check that doc comments start with the symbol name",
+	URL:  "https://github.com/maciej/docstartsym",
 	Run:  run,
 }
 
 func run(pass *analysis.Pass) (any, error) {
 	for _, file := range pass.Files {
 		for _, decl := range file.Decls {
-			if funcDecl, ok := decl.(*ast.FuncDecl); ok {
-				if funcDecl.Doc == nil {
+			switch d := decl.(type) {
+			case *ast.FuncDecl:
+				if d.Doc == nil {
 					continue
 				}
 
 				// Get the first line of the documentation
-				docText := funcDecl.Doc.Text()
+				docText := d.Doc.Text()
 				firstLine := strings.Split(docText, "\n")[0]
 
 				// Check if the first line starts with the function name
-				if !strings.HasPrefix(strings.TrimSpace(firstLine), funcDecl.Name.Name) {
-					pass.Reportf(funcDecl.Doc.Pos(),
-						"documentation should start with the function name %q",
-						funcDecl.Name.Name)
+				if !strings.HasPrefix(strings.TrimSpace(firstLine), d.Name.Name) {
+					pass.Reportf(d.Doc.Pos(),
+						"doc comment should start with the function name %q",
+						d.Name.Name)
+				}
+
+			case *ast.GenDecl:
+				if d.Doc == nil {
+					continue
+				}
+
+				// Get the first line of the documentation
+				docText := d.Doc.Text()
+				firstLine := strings.Split(docText, "\n")[0]
+
+				for _, spec := range d.Specs {
+					switch s := spec.(type) {
+					case *ast.TypeSpec:
+						// Handle type declarations
+						if !strings.HasPrefix(strings.TrimSpace(firstLine), s.Name.Name) {
+							pass.Reportf(d.Doc.Pos(),
+								"doc comment should start with the type name %q",
+								s.Name.Name)
+						}
+
+					case *ast.ValueSpec:
+						// Handle var and const declarations
+						for _, name := range s.Names {
+							if !strings.HasPrefix(strings.TrimSpace(firstLine), name.Name) {
+								pass.Reportf(d.Doc.Pos(),
+									"doc comment should start with the %s name %q",
+									getDeclType(d.Tok), name.Name)
+							}
+						}
+					}
 				}
 			}
 		}
 	}
 
 	return nil, nil
+}
+
+func getDeclType(tok token.Token) string {
+	switch tok {
+	case token.VAR:
+		return "variable"
+	case token.CONST:
+		return "constant"
+	default:
+		return "symbol"
+	}
 }
--- pkg/analyzer/testdata/src/a/a.go
+++ pkg/analyzer/testdata/src/a/a.go
@@ -1,7 +1,25 @@
 package a
 
-// Incorrect doc does something // want `documentation should start with the function name "IncorrectDoc"`
+// Incorrect doc does something // want `doc comment should start with the function name "IncorrectDoc"`
 func IncorrectDoc() {}
 
 // CorrectDoc starts with the function name
 func CorrectDoc() {}
+
+// Incorrect Type is a type // want `doc comment should start with the type name "IncorrectType"`
+type IncorrectType struct{}
+
+// CorrectType starts with the type name
+type CorrectType struct{}
+
+// Incorrect Var is a variable // want `doc comment should start with the variable name "IncorrectVar"`
+var IncorrectVar int
+
+// CorrectVar starts with the variable name
+var CorrectVar int
+
+// Incorrect Const is a constant // want `doc comment should start with the constant name "IncorrectConst"`
+const IncorrectConst = 1
+
+// CorrectConst starts with the constant name
+const CorrectConst = 1
